15c996f92b7cc894634358ffe89c56deb8088468
Eitan Zelditch <1100083210@edu.cbe.ab.ca>
2024-10-23 15:25:49 -0600

    Finish finding random word && gamestate

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ server.use(express.static("public"));
 
 //All your code goes here
 let activeSessions = {};
+let randomWord;
+async function randomWordGen() {
+  let response = await fetch(
+    "https://random-word-api.herokuapp.com/word?number=1&length=5"
+  );
+  let result = await response;
+  randomWord = result;
+}
 
 server.get("/newgame", (req, res) => {
   let newID = uuid.v4();
   let newGame = {
-    wordToGuess: "spike",
+    wordToGuess: randomWord,
     guesses: [],
     wrongLetters: [],
     closeLetters: [],
@@ -22,6 +30,14 @@ server.get("/newgame", (req, res) => {
   res.status(201);
   res.send({ sessionID: newID });
   console.log(activeSessions);
+});
+
+server.get("/gamestate", (req, res) => {
+  let gameState = {
+    wordToGuess: randomWord,
+    guesses: [],
+  };
+  res.status(200);
 });
 
 //Do not remove this line. This allows the test suite to start
